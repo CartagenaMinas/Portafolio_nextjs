3ce4707ca5b6929dfc19e395dccf57ac236db5a3
CartagenaMinas <79287172+CartagenaMinas@users.noreply.github.com>
2021-05-29 22:32:53 -0500

    V2
    Cambio Video1

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import { Image } from "@chakra-ui/react";
+
 export default function Home() {
-  return <div> Hola </div>;
+  return (
+    <Image
+  borderRadius="full"
+  boxSize="150px"
+  src="https://bit.ly/sage-adebayo"
+  alt="Segun Adebayo"
+/>
+ );
 }
