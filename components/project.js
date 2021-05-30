b71b4e568df7a474ef9e73fe408da115989fc6cc
CartagenaMinas <79287172+CartagenaMinas@users.noreply.github.com>
2021-05-30 03:49:59 -0500

    Update project.js
    v3 dia 1

--- components/project.js
+++ components/project.js
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/layout";
 
 export default function Project({text}) {
     return (
-        <Box w="full" h="100px" _hover={{background: 'rgba(255,255,255,0.3)'}}>
+        <Box w="full" h="100px" _hover={{background: 'rgba(255,255,255,0.2)'}}>
             {text}
         </Box>
     );
